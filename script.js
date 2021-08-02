11014352f87063bb8cb546a98e5640cd3ac3c53d
Stefan Sinca <stefisinca@gmail.com>
2021-08-02 22:01:16 +0300

    Created a randomNumber functions and a switch that log to console the info based on the random number.

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log(`Have trust in no one!`)
+
+const randomNumber = () => {
+   let x =  Math.floor(Math.random( ) * 10);
+    switch(x) {
+        case 0:
+        console.log('I am swift as a gazelle. An old one. With arthritis. Run over by a Land Rover. 8 days ago.');
+        break;
+        case 1: 
+            console.log(`I asked God for a bike, but I know God doesn’t work that way. So I stole a bike and asked for forgiveness.`);
+            break;
+            case 2:
+                console.log(`I didn’t fall down. I did attack the floor, though.`);
+                break;
+                case 3:
+                    console.log(`I don’t need a hair stylist, my pillow gives me a new hairstyle every morning.`);
+                    break;
+                    case 4:
+                        console.log(`I don’t suffer from insanity, I enjoy every minute of it.`);
+                        break;
+                        case 5:
+                            console.log(`I get enough exercise pushing my luck.`);
+                            case 6:
+                                console.log(`I had loads to do today. Ah well, so now I have loads to do tomorrow.`);
+                                break;
+                                case 7:
+                                    console.log(`I like to be an optimist. It pisses people off.`);
+                                    break;
+                                    case 8:
+                                        console.log(`I’m going to have a seriously exciting December!`);
+                                        case 9:
+                                            console.log(`I refuse to have a battle of wits with an opponent so clearly unarmed.`);
+                                            break;
+                                            case 10:
+                                                console.log(`After millions of years of evolution, you’re kind of a disappointment.`);
+                                                break;
+                                                default:
+                                                    console.log(`Please STOP! I'm to funny!`)
+    }}
+
+    randomNumber()
